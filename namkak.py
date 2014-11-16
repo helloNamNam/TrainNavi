91f09019422c414b889573ea7c31d25bf3d094e7
Zearra <gamemer_88@hotmail.com>
2014-11-16 12:42:08 +0700

    Update namkak.py
    หึหึ

--- namkak.py
+++ namkak.py
@@ -6,19 +6,26 @@ class Py:
         self.world.geometry("800x450")
         self.world.title("Train navigator")
 
-        self.draw = Canvas(self.world,width = 400, height = 200)
-        self.draw.pack()
+        self.draw = Canvas(self.world,width = 550, height = 450, bg = 'Gray')
+        self.draw.pack(side = LEFT)
 
-        self.b = Button(self.draw, text = "asdfghfd", command = self.button)
-        self.b.grid(row = 0,column = 0 )
+        self.count = Canvas(self.world, width = 250, height = 450, bg = 'Blue')
+        self.count.pack(side = RIGHT)
 
-        self.b = Button(self.draw, text = "asdfghfd", command = self.button)
-        self.b.grid(row = 0,column = 1)
+        self.station = ""
+        self.label = Label(self.count, text = self.station, bg = 'Blue')
+        self.label.place(x = 100, y = 100)
+
+
+        self.b = Button(self.draw, text = "click", command = self.button)
+        self.b.place(x = 200,y = 100 )
 
 
         self.world.mainloop()
 
     def button(self):
-        print("")
+        self.station = "sdfgd"
+        self.label = Label(self.count, text = self.station)
+        self.label.place(x = 100, y = 100)
 
 Py()

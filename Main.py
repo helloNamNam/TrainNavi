45a0dd3172416d49093675b45046bddd8b8b6eb3
Napat Tunglukmongkol <helloNamNam@users.noreply.github.com>
2014-12-17 13:04:22 +0700

    Update and rename THIS IS IT !! to Main.py
    Finished Code

--- Main.py
+++ Main.py
@@ -5,11 +5,11 @@ class Interface(object):
     def __init__(self):
         """ main method """
         self.root = Tk()
-        self.root.geometry("800x600+400+200")
+        self.root.geometry("800x600+400+50")
         self.root.title("Train Navigator")
 
         #__Background__#
-        photo = ImageTk.PhotoImage(Image.open("Train.jpg"))
+        photo = ImageTk.PhotoImage(Image.open("Image/Train.jpg"))
         bg_inter = Label(self.root, image = photo)
         bg_inter.place(x=0, y=0)
 
@@ -91,7 +91,10 @@ class Station_bts(object):
         bg_bts.place(x=0, y=0)
 
         #__Option Menu__#
-        self.station_list = ['Mo-Shit', 'Payathai']
+        self.station_list = ['Bang wa','Wutthakat','Talat Phlu','Pho nimit','Wongwian Yai','Krung Thon Buri',\
+                              'Saphan Taksin','Surasak','Chong Nonsi','Sala Daeng','Ratchadamri','Siam(Silom)','National Stadium', \
+                              '---Bearing---','Bang Na','Udom Suk','Punnawithi','Bang Chak','On nut','Phra Khanong','Ekkamai','Thong Lo','Phrom Phong',\
+                              'Asok','Nana','Phloan Chit','Chit Lom','Siam(Sukumvit)','Ratchathewi','Phaya Thai','Victory Monument','Sanam Pao','Ari','Saphan Khwai','Mo chit']
         self.station_one = StringVar()
         self.station_two = StringVar()
         self.option_sta_one = OptionMenu(self.root, self.station_one, *self.station_list
@@ -122,10 +125,26 @@ class Station_bts(object):
 
     def bts_money(self):
         """ calculate money """
-        bts = {0:0, 1:15}
-        first = self.station_list.index(self.station_one.get())
-        second = self.station_list.index(self.station_two.get())
-        distance = abs(first - second)
+        train_s = ['Bang wa','Wutthakat','Talat Phlu','Pho nimit','Wongwian Yai','Krung Thon Buri',\
+                   'Saphan Taksin','Surasak','Chong Nonsi','Sala Daeng','Ratchadamri','Siam(Silom)','National Stadium']
+        train_k = ['---Bearing---','Bang Na','Udom Suk','Punnawithi','Bang Chak','On nut','Phra Khanong','Ekkamai','Thong Lo','Phrom Phong',\
+                   'Asok','Nana','Phloan Chit','Chit Lom','Siam(Sukumvit)','Ratchathewi','Phaya Thai','Victory Monument','Sanam Pao','Ari','Saphan Khwai','Mo chit']
+        bts = {0:15, 1:15, 2:25, 3:28, 4:31, 5:34, 6:37, 7:42, 8:42, 9:42, 10:42, \
+               11:42, 12:42, 13:42, 14:42, 15:42, 16:42, 17:52}
+        start = self.station_one.get()
+        stop = self.station_two.get()
+        if (start in train_s and stop not in train_s) or (start in train_k and stop not in train_k) :
+            if start in train_s:
+                distance = abs(train_s.index(start) - train_s.index('Siam(Silom)'))
+                distance += abs(train_k.index('Siam(Sukumvit)') - train_k.index(stop))
+            else:
+                distance = abs(train_k.index(start) - train_k.index('Siam(Sukumvit)'))
+                distance += abs(train_s.index('Siam(Silom)') - train_s.index(stop))
+        else:
+            if start in train_s:
+                distance = abs(train_s.index(start) - train_s.index(stop))
+            else:
+                distance = abs(train_k.index(start) - train_k.index(stop))
         if distance >= 17:
             self.result_box.set(bts[17])
         else:
@@ -137,12 +156,12 @@ class Station_bts(object):
         Main()
 
 class Station_mrt(object):
-    """ BTS class to select station """
+    """ MRT class to select station """
     def __init__(self):
         """ main method """
         self.root = Tk()
         self.root.geometry("800x600+400+100")
-        self.root.title("BTS Stations")
+        self.root.title("MRT Stations")
 
         #__Background__#
         photo = ImageTk.PhotoImage(Image.open("Image/Mrt.jpg"))
@@ -150,24 +169,33 @@ class Station_mrt(object):
         bg_mrt.place(x=0, y=0)
 
         #__Option Menu__#
-        self.station_list = ['test']
+        self.station_list = ['Hua Lamphong', 'Sam Yan', 'Si Lom', 'Lumphini', 'Khlong Toei', \
+                            'Queen Sirikit National Conv Center', 'Phetchburi', 'Phra Ram 9', 'Thailand Cultural Centre', \
+                            'Huai Khwang', 'Sutthisan', 'Ratchadaphisek', 'Lat Phrao', 'Phahon Yothin', 'Chatuchak Park', \
+                            'Kamphaeng Phet', 'Bang Sue']
         self.station_one = StringVar()
         self.station_two = StringVar()
         self.option_sta_one = OptionMenu(self.root, self.station_one, *self.station_list
-                                         ).place(x=100, y=100)
+                                         ).place(x=150, y=188)
         self.option_sta_two = OptionMenu(self.root, self.station_two, *self.station_list
-                                         ).place(x=100, y=200)
+                                         ).place(x=150, y=228)
         self.station_one.set(self.station_list[0])
         self.station_two.set(self.station_list[0])
+        label_text1 = Label(self.root, text = "Select station to start", bg = "#d9fdf9"
+                            ).place(x=10, y=188)
+        label_text2 = Label(self.root, text = "Select station end", bg = "#d9fdf9"
+                            ).place(x=15, y=228)
 
         #__Entry Box__#
         self.result_box = StringVar()
-        Entry(self.root, textvariable = self.result_box, bg = "white"
-              ).place(x=200, y=300)
+        Entry(self.root, width = 10, textvariable = self.result_box, bg = "white"
+              ).place(x=70, y=308)
+        label_text3 = Label(self.root, text = "Price :").place(x=20, y=310)
+        label_text4 = Label(self.root, text = "Baht").place(x=170, y=310)
 
         #__Button__#
         self.button_mrt = Button(self.root, text = "Confirm", bg = "white",
-                                 command = self.mrt_money).place(x=0, y=0)
+                                 command = self.mrt_money).place(x=140, y=268)
         self.button_back = Button(self.root, text = "Back", bg = "white",
                                   command = self.back_method).place(x=5, y=560)
 
@@ -175,8 +203,9 @@ class Station_mrt(object):
 
     def mrt_money(self):
         """ calculate money """
-        first = self.station_list.index(self.station_one)
-        second = self.station_list.index(self.station_two)
+        mrt = {0:16, 1:16, 2:18, 3:20, 4:22, 5:25, 6:27, 7:29, 8:31, 9:34, 10:36, 11:38, 12:40}
+        first = self.station_list.index(self.station_one.get())
+        second = self.station_list.index(self.station_two.get())
         distance = abs(first - second)
         if distance >= 12:
             self.result_box.set(mrt[12])
@@ -189,12 +218,12 @@ class Station_mrt(object):
         Main()
 
 class Station_arl(object):
-    """ BTS class to select station """
+    """ arl class to select station """
     def __init__(self):
         """ main method """
         self.root = Tk()
         self.root.geometry("800x600+400+100")
-        self.root.title("BTS Stations")
+        self.root.title("arl Stations")
 
         #__Background__#
         photo = ImageTk.PhotoImage(Image.open("Image/Air.jpg"))
@@ -202,24 +231,31 @@ class Station_arl(object):
         bg_arl.place(x=0, y=0)
 
         #__Option Menu__#
-        self.station_list = ['test']
+        self.station_list = arl_stationlist = ['Suvarnabhumi', 'Lat Krabang', 'Ban Thap Chang', 'Hua Mak', 'Ramkhamhaeng', 'Makkasan', \
+                                              'Ratchaprarop', 'Phaya Thai']
         self.station_one = StringVar()
         self.station_two = StringVar()
         self.option_sta_one = OptionMenu(self.root, self.station_one, *self.station_list
-                                         ).place(x=100, y=100)
+                                         ).place(x=350, y=8)
         self.option_sta_two = OptionMenu(self.root, self.station_two, *self.station_list
-                                         ).place(x=200, y=100)
+                                         ).place(x=350, y=48)
         self.station_one.set(self.station_list[0])
         self.station_two.set(self.station_list[0])
+        label_text1 = Label(self.root, text = "Select station to start", bg = "#dafefa"
+                            ).place(x=200, y=10)
+        label_text2 = Label(self.root, text = "Select station end", bg = "#dafefa"
+                            ).place(x=220, y=50)
 
         #__Entry Box__#
         self.result_box = StringVar()
-        Entry(self.root, textvariable = self.result_box, bg = "white"
-              ).place(x=100, y=300)
+        Entry(self.root,width = 10,  textvariable = self.result_box, bg = "white"
+              ).place(x=250, y=130)
+        label_text3 = Label(self.root, text = "Price :").place(x=200, y=132)
+        label_text4 = Label(self.root, text = "Baht").place(x=350, y=132)
 
         #__Button__#
         self.button_arl = Button(self.root, text = "Confirm", bg = "white",
-                                 command = self.arl_money).place(x=100, y=350)
+                                 command = self.arl_money).place(x=340, y=90)
         self.button_back = Button(self.root, text = "Back", bg = "white",
                                   command = self.back_method).place(x=5, y=560)
 
@@ -227,8 +263,9 @@ class Station_arl(object):
 
     def arl_money(self):
         """ calculate money """
-        first = self.station_list.index(self.station_one)
-        second = self.station_list.index(self.station_two)
+        arl = {0:15, 1:15, 2:20, 3:25, 4:30, 5:35, 6:40, 7:45}
+        first = self.station_list.index(self.station_one.get())
+        second = self.station_list.index(self.station_two.get())
         distance = abs(first - second)
         self.result_box.set(arl[distance])
 
